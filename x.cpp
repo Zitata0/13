85c3f199e8275ce2b557d493f9ba51f467fdc3fc
Zitata0 <93836641+Zitata0@users.noreply.github.com>
2021-11-18 20:57:53 +0300

    Update x.cpp

--- x.cpp
+++ x.cpp
@@ -113,7 +113,7 @@ int main() {
 			system("cls");
 			cout << "[+] Результат:\n";
 			for (int i = 0; i < x; i++) {
-				for (int i = 0; i < z * 2 - 2; i++)
+				for (int i2 = 0; i2 < z * 2 - 2; i2++)
 					cout << y;
 				cout << endl;
 			}
